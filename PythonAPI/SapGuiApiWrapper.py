c6c3b81e51ad2c4c6f6c163879501e9577fb72d4
Nathan Almeida <neitan96@outlook.com>
2023-09-20 14:10:41 -0300

    Feita a classe GuiCheckBox para Python

--- PythonAPI/SapGuiApiWrapper.py
+++ PythonAPI/SapGuiApiWrapper.py
@@ -23,6 +23,7 @@ OK:
 	- GuiContextMenu
 	- GuiBox
 	- GuiButton
+	- GuiCheckBox
 
 TODO Components:
 
@@ -39,7 +40,6 @@ TODO Components:
 	- GuiBarChart
 	- GuiCalendar
 	- GuiChart
-	- GuiCheckBox
 	- GuiColorSelector
 	- GuiComboBox
 	- GuiComboBoxControl
@@ -582,6 +582,81 @@ class SapGuiVComponent(SapGuiComponent):
         ''' Altura do componente em pixels.
         '''
         return self.component.Height
+
+class SapGuiCheckBox(SapGuiVComponent):
+    #TODO Fazer uma descrição
+    
+    def GetListProperty(self, property: str) -> str:
+        ''' Para mais informações consulte a documentação sobre o método GetListProperty dentro do GuiLabel Object.
+        '''
+        return self.component.GetListProperty(property)
+    
+    def GetListPropertyNonRec(self, property: str) -> str:
+        ''' Este método retorna informações que são compiladas no servidor para aprimorar as listas ABAP com informações de acessibilidade.
+        Consulte GuiLabel::GetListProperty para obter uma descrição dos atributos disponíveis.
+        Em contraste com o método GetListProperty, GetListPropertyNonRec retornará apenas informações definidas para o elemento específico e
+        ignorará as propriedades da lista definidas para elementos pai.
+        '''
+        return self.component.GetListPropertyNonRec(property)
+    
+    def ColorIndex(self) -> int:
+        ''' Este número define o índice da cor da lista deste elemento.
+        '''
+        return self.component.ColorIndex
+    
+    def ColorIntensified(self) -> bool:
+        ''' Esta propriedade será True se o sinalizador Intensificado estiver definido no Screen Painter para este elemento dynpro.
+        '''
+        return self.component.ColorIntensified
+    
+    def ColorInverse(self) -> bool:
+        ''' Esta propriedade será True se o estilo de cor inverso estiver definido no Screen Painter para o elemento.
+        '''
+        return self.component.ColorInverse
+    
+    def Flushing(self) -> bool:
+        ''' Alguns componentes, como botões de opção ou caixas de seleção, podem causar uma viagem de ida e
+        volta quando seu valor for alterado. Se for esse o caso, a propriedade Flushing é True.
+        '''
+        return self.component.Flushing
+    
+    def IsLeftLabel(self) -> bool:
+        ''' Esta propriedade é True se o componente tiver o sinalizador 'atribuir à esquerda'.
+        '''
+        return self.component.IsLeftLabel
+    
+    def IsListElement(self) -> bool:
+        ''' Esta propriedade é True se o elemento estiver em uma lista ABAP e não em uma tela dynpro.
+        '''
+        return self.component.IsListElement
+    
+    def IsRightLabel(self) -> bool:
+        ''' Esta propriedade é True se o componente tiver o sinalizador 'assign right'.
+        '''
+        return self.component.IsRightLabel
+    
+    def LeftLabel(self) -> bool:
+        ''' Etiqueta esquerda do componente. O rótulo é atribuído no Screen Painter, usando o sinalizador 'assign left'.
+        '''
+        return self.component.LeftLabel
+    
+    def RightLabel(self) -> bool:
+        ''' Etiqueta direita do componente.
+        Esta propriedade é definida no Screen Painter usando o sinalizador 'assign right'.
+        '''
+        return self.component.RightLabel
+    
+    def RowText(self) -> bool:
+        ''' Esta propriedade só está disponível em telas de lista ABAP.
+        Ele retorna o texto da linha while que contém o componente atual.
+        '''
+        return self.component.RowText
+    
+    def Selected(self) -> bool:
+        ''' Assim como os botões de opção, marcar uma caixa de seleção pode
+        causar comunicação com o servidor, dependendo da definição do ABAP Screen Painter.
+        '''
+        return self.component.Selected
 
 class SapGuiButton(SapGuiVComponent):
     ''' GuiButton representa todos os botões que estão no dynpros, na barra de ferramentas ou nos controles da tabela.
