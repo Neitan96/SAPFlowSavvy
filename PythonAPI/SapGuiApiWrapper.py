9f8df1c14e2d45bcaad6ff267e3c74881e0998f7
Nathan Almeida <neitan96@outlook.com>
2023-09-21 12:09:27 -0300

    Feita a classe GuiChart para Python

--- PythonAPI/SapGuiApiWrapper.py
+++ PythonAPI/SapGuiApiWrapper.py
@@ -34,6 +34,7 @@ OK:
 	- GuiComboBoxEntry
 	- GuiComboBoxControl
 	- GuiSapChart
+	- GuiChart
 
 * Enumerations
 	- GuiComponentType
@@ -71,7 +72,6 @@ Priority:
 	- GuiApoGrid
 	- GuiBarChart
 	- GuiCalendar
-	- GuiChart
 	- GuiColorSelector
 	- GuiDialogShell
 	- GuiEAIViewer2D
@@ -1448,6 +1448,32 @@ class SapGuiShell(SapGuiVContainer):
         ''' Informações adicionais de tipo para identificar o controle representado pelo shell, por exemplo Picture, TextEdit, GridView…
         '''
         return self.component.SubType
+
+class SapGuiChart(SapGuiShell):
+    ''' O objeto GuiChart é de natureza muito técnica. Deve ser utilizado apenas para gravação e reprodução,
+    pois a maioria dos parâmetros não pode ser determinada de outra forma.
+    '''
+    
+    def ValueChange(self, series: int, point: int, x_value: str, y_value: str, data_change: bool, id: str, z_value: str, change_flag: int):
+        '''
+        Série: Número do conjunto de dados dentro da linha que deve ser alterado.
+        point: Número do ponto de dados na linha que deve ser alterado.
+        x_value: Novo valor de x.
+        y_value: Novo valor de y.
+        data_change: Definir este parâmetro como True significa que o valor não foi alterado interativamente no gráfico,
+            mas sim inserindo o novo valor na página de propriedades do DataPoint.
+        id: ID do contêiner de dados GFW do ponto alterado. Pode ser usado em vez do par série/ponto.
+        z_value: Novo valor z.
+        ChangeFlag: Notifica qual valor foi alterado ou se foi um valor de tempo.
+            O valor é definido como uma matriz de bits, usando os 5 bits inferiores.
+            1 x
+            2 y
+            4 x é o valor do tempo
+            8 y é o valor do tempo
+            16 z
+            Se o novo valor for um momento específico, ele deverá ser definido usando uma string no formato mm/dd/aaaa hh:mm:ss.
+        '''
+        self.component.ValueChange(series, point, x_value, y_value, data_change, id, z_value, change_flag)
 
 class SapGuiSapChart(SapGuiShell):
     # TODO Verificar classe
