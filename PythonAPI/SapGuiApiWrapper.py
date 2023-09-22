8cd9fe9846e63028529badbd1db3d02aab5db706
Nathan Almeida <neitan96@outlook.com>
2023-09-21 21:37:44 -0300

    Feita a classe GuiTree para Python

--- PythonAPI/SapGuiApiWrapper.py
+++ PythonAPI/SapGuiApiWrapper.py
@@ -40,6 +40,7 @@ OK:
 	- GuiTitlebar
 	- GuiStatusbar
 	- GuiStatusPane
+	- GuiTree
 
 * Enumerations
 	- GuiComponentType
@@ -65,7 +66,6 @@ Priority:
 	- GuiTextedit
 	- GuiTextField
 	- GuiToolbarControl
-	- GuiTree
 
 * Objects:
 	- GuiAbapEditor - N/a
@@ -1554,6 +1554,441 @@ class SapGuiShell(SapGuiVContainer):
         ''' Informações adicionais de tipo para identificar o controle representado pelo shell, por exemplo Picture, TextEdit, GridView…
         '''
         return self.component.SubType
+
+class SapGuiTree(SapGuiShell):
+    ''' O Tree Control oferece suporte a três tipos de árvore.
+    '''
+    
+    def ChangeCheckbox(self, node_key: str, item_name: str, checked: bool) -> None:
+        ''' Emula a mudança de estado de uma caixa de seleção.
+        '''
+        self.component.ChangeCheckbox(node_key, item_name, checked)
+
+    def ClickLink(self, node_key: str, item_name: str) -> None:
+        ''' Emula a ativação de um link.
+        '''
+        self.component.ClickLink(node_key, item_name)
+
+    def CollapseNode(self, node_key: str) -> None:
+        ''' Fecha o nó com a chave de nó especificada.
+        '''
+        self.component.CollapseNode(node_key)
+
+    def DefaultContextMenu(self) -> None:
+        ''' Solicita um menu de contexto para todo o Controle de Árvore.
+        '''
+        self.component.DefaultContextMenu()
+
+    def DoubleClickItem(self, node_key: str, item_name: str) -> None:
+        ''' Emula o duplo clique em um item de texto.
+        '''
+        self.component.DoubleClickItem(node_key, item_name)
+
+    def DoubleClickNode(self, node_key: str) -> None:
+        ''' Emula o duplo clique em um nó.
+        '''
+        self.component.DoubleClickNode(node_key)
+
+    def EnsureVisibleHorizontalItem(self, node_key: str, item_name: str) -> None:
+        ''' Desloca a Árvore horizontalmente até que o item seja visível.
+        '''
+        self.component.EnsureVisibleHorizontalItem(node_key, item_name)
+
+    def ExpandNode(self, node_key: str) -> None:
+        ''' Expande o nó com a chave de nó especificada.
+        '''
+        self.component.ExpandNode(node_key)
+
+    def FindNodeKeyByPath(self, path: str) -> str:
+        ''' Encontra a chave de nó pelo seu caminho.
+        '''
+        return self.component.FindNodeKeyByPath(path)
+
+    def GetAbapImage(self, key: str, name: str) -> str:
+        ''' Recupera o código do ícone de uma imagem exibida no item especificado.
+        '''
+        return self.component.GetAbapImage(key, name)
+
+    def GetAllNodeKeys(self):
+        ''' Retorna uma coleção de todas as chaves de nó presentes no Controle de Árvore.
+        '''
+        return self.component.GetAllNodeKeys()
+
+    def GetCheckBoxState(self, node_key: str, item_name: str) -> int:
+        ''' Recupera o estado da caixa de seleção (1 = Marcado, 0 = Desmarcado).
+        '''
+        return self.component.GetCheckBoxState(node_key, item_name)
+
+    def GetColumnCol(self, col_name: str):
+        ''' Retorna as chaves de todos os itens na coluna especificada.
+        '''
+        return self.component.GetColumnCol(col_name)
+
+    def GetColumnHeaders(self):
+        ''' Retorna uma coleção de cabeçalhos de coluna.
+        '''
+        return self.component.GetColumnHeaders()
+
+    def GetColumnIndexFromName(self, key: str) -> int:
+        ''' Retorna o índice da coluna (começando em 1) da coluna especificada.
+        '''
+        return self.component.GetColumnIndexFromName(key)
+
+    def GetColumnNames(self):
+        ''' Retorna uma coleção de nomes de coluna.
+        '''
+        return self.component.GetColumnNames()
+
+    def GetColumnTitleFromName(self, key: str) -> str:
+        ''' Retorna o título da coluna especificada pelo parâmetro.
+        '''
+        return self.component.GetColumnTitleFromName(key)
+
+    def GetColumnTitles(self):
+        ''' Retorna uma coleção de títulos de coluna.
+        '''
+        return self.component.GetColumnTitles()
+
+    def GetFocusedNodeKey(self) -> str:
+        ''' Retorna a chave do nó que está com foco.
+        '''
+        return self.component.GetFocusedNodeKey()
+
+    def GetHierarchyLevel(self, key: str) -> int:
+        ''' Retorna o nível hierárquico da chave especificada, começando no nível 0.
+        '''
+        return self.component.GetHierarchyLevel(key)
+
+    def GetHierarchyTitle(self) -> str:
+        ''' Retorna o título hierárquico.
+        '''
+        return self.component.GetHierarchyTitle()
+
+    def GetIsDisabled(self, node_key: str, item_name: str) -> int:
+        ''' Retorna se o item especificado está desativado (1 = Desativado, 0 = Não desativado).
+        '''
+        return self.component.GetIsDisabled(node_key, item_name)
+
+    def GetIsHighLighted(self, node_key: str, item_name: str) -> int:
+        ''' Retorna se o item especificado está destacado (1 = Destacado, 0 = Não destacado).
+        '''
+        return self.component.GetIsHighLighted(node_key, item_name)
+
+    def GetItemHeight(self, node_key: str, item_name: str) -> int:
+        ''' Recupera a altura do item especificado em pixels.
+        '''
+        return self.component.GetItemHeight(node_key, item_name)
+
+    def GetItemLeft(self, node_key: str, item_name: str) -> int:
+        ''' Recupera a posição esquerda do item especificado em pixels.
+        '''
+        return self.component.GetItemLeft(node_key, item_name)
+
+    def GetItemStyle(self, node_key: str, item_name: str) -> int:
+        ''' Recupera o estilo do item especificado.
+        '''
+        return self.component.GetItemStyle(node_key, item_name)
+
+    def GetItemText(self, key: str, name: str) -> str:
+        ''' Retorna o texto do item especificado pelos parâmetros chave e nome.
+        '''
+        return self.component.GetItemText(key, name)
+
+    def GetItemTextColor(self, key: str, name: str) -> int:
+        ''' Recupera a cor da fonte do item especificado.
+        '''
+        return self.component.GetItemTextColor(key, name)
+
+    def GetItemToolTip(self, key: str, name: str) -> str:
+        ''' Recupera a dica do item especificado.
+        '''
+        return self.component.GetItemToolTip(key, name)
+
+    def GetItemTop(self, node_key: str, item_name: str) -> int:
+        ''' Recupera a posição superior do item especificado em pixels.
+        '''
+        return self.component.GetItemTop(node_key, item_name)
+
+    def GetItemType(self, key: str, name: str) -> int:
+        ''' Recupera o tipo de item da árvore de colunas:
+        trvTreeStructureHierarchy = 0
+        trvTreeStructureImage = 1
+        trvTreeStructureText = 2
+        trvTreeStructureBool = 3
+        trvTreeStructureButton = 4
+        trvTreeStructureLink = 5
+        '''
+        return self.component.GetItemType(key, name)
+
+    def GetItemWidth(self, node_key: str, item_name: str) -> int:
+        ''' Recupera a largura do item especificado em pixels.
+        '''
+        return self.component.GetItemWidth(node_key, item_name)
+
+    def GetListTreeNodeItemCount(self, node_key: str) -> int:
+        ''' Retorna o número de itens visíveis do nó especificado em uma árvore de lista.
+        '''
+        return self.component.GetListTreeNodeItemCount(node_key)
+
+    def GetNextNodeKey(self, node_key: str) -> str:
+        ''' Retorna a chave do próximo nó pertencente ao mesmo nó um nível acima.
+        '''
+        return self.component.GetNextNodeKey(node_key)
+
+    def GetNodeAbapImage(self, key: str) -> str:
+        ''' Recupera o código de ícone do nó especificado.
+        '''
+        return self.component.GetNodeAbapImage(key)
+
+    def GetNodeChildrenCount(self, key: str) -> int:
+        ''' Retorna o número de filhos diretos visíveis do nó especificado.
+        '''
+        return self.component.GetNodeChildrenCount(key)
+
+    def GetNodeChildrenCountByPath(self, path: str) -> int:
+        ''' Retorna o número de filhos visíveis do nó especificado pelo caminho.
+        '''
+        return self.component.GetNodeChildrenCountByPath(path)
+
+    def GetNodeHeight(self, key: str) -> int:
+        ''' Retorna a altura do nó especificado em pixels.
+        '''
+        return self.component.GetNodeHeight(key)
+
+    def GetNodeIndex(self, key: str) -> int:
+        ''' Retorna o índice da chave especificada dentro do seu nó.
+        '''
+        return self.component.GetNodeIndex(key)
+
+    def GetNodeItemHeaders(self, node_key: str):
+        ''' Retorna um objeto com os cabeçalhos de item do nó especificado.
+        '''
+        return self.component.GetNodeItemHeaders(node_key)
+
+    def GetNodeKeyByPath(self, path: str) -> str:
+        ''' Chave do nó especificado pelo caminho dado.
+        '''
+        return self.component.GetNodeKeyByPath(path)
+
+    def GetNodeLeft(self, key: str) -> int:
+        ''' Retorna a posição esquerda do nó especificado em pixels.
+        '''
+        return self.component.GetNodeLeft(key)
+
+    def GetNodePathByKey(self, key: str) -> str:
+        ''' Dado uma chave de nó, o caminho é recuperado (por exemplo, "2\1\2").
+        '''
+        return self.component.GetNodePathByKey(key)
+
+    def GetNodesCol(self):
+        ''' A coleção contém as chaves de todos os nós na árvore.
+        '''
+        return self.component.GetNodesCol()
+
+    def GetNodeStyle(self, node_key: str) -> int:
+        ''' Recupera o estilo do nó especificado.
+        '''
+        return self.component.GetNodeStyle(node_key)
+
+    def GetNodeTextByKey(self, key: str) -> str:
+        ''' Retorna o texto do nó especificado pela chave dada.
+        '''
+        return self.component.GetNodeTextByKey(key)
+
+    def GetNodeTextByPath(self, path: str) -> str:
+        ''' O texto de um nó definido pelo caminho dado é retornado.
+        '''
+        return self.component.GetNodeTextByPath(path)
+
+    def GetNodeTextColor(self, key: str) -> int:
+        ''' Retorna a cor da fonte do nó especificado.
+        '''
+        return self.component.GetNodeTextColor(key)
+
+    def GetNodeToolTip(self, node_key: str) -> str:
+        ''' Retorna a dica de ferramenta do nó especificado.
+        '''
+        return self.component.GetNodeToolTip(node_key)
+
+    def GetNodeTop(self, key: str) -> int:
+        ''' Retorna a posição superior do nó especificado em pixels.
+        '''
+        return self.component.GetNodeTop(key)
+
+    def GetNodeWidth(self, key: str) -> int:
+        ''' Retorna a largura do nó especificado em pixels.
+        '''
+        return self.component.GetNodeWidth(key)
+
+    def GetParent(self, ckey: str) -> str:
+        ''' Chave do nó pai do nó especificado pela chave dada.
+        '''
+        return self.component.GetParent(ckey)
+
+    def GetPreviousNodeKey(self, node_key: str) -> str:
+        ''' Retorna a chave do nó anterior pertencente ao mesmo nó um nível acima.
+        '''
+        return self.component.GetPreviousNodeKey(node_key)
+
+    def GetSelectedNodes(self):
+        ''' Retorna uma coleção de nós selecionados.
+        '''
+        return self.component.GetSelectedNodes()
+
+    def GetSelectionMode(self) -> int:
+        ''' Retorna o modo de seleção do Controle de Árvore:
+        0: Seleção de Nó Único
+        1: Seleção de Múltiplos Nós
+        2: Seleção de Item Único
+        3: Seleção de Múltiplos Itens
+        '''
+        return self.component.GetSelectionMode()
+
+    def GetStyleDescription(self, n_style: int) -> str:
+        ''' Retorna a descrição do estilo especificado.
+        '''
+        return self.component.GetStyleDescription(n_style)
+
+    def GetSubNodesCol(self, path: str):
+        ''' Retorna uma coleção de chaves de todos os subníveis do nó especificado pela chave dada.
+        '''
+        return self.component.GetSubNodesCol(path)
+
+    def GetTreeType(self) -> int:
+        ''' Retorna o tipo de árvore:
+        0: Árvore Simples
+        1: Árvore de Lista
+        2: Árvore de Coluna
+        '''
+        return self.component.GetTreeType()
+
+    def HeaderContextMenu(self, header_name: str) -> None:
+        ''' Solicita um menu de contexto para um cabeçalho.
+        '''
+        self.component.HeaderContextMenu(header_name)
+
+    def IsFolder(self, node_key: str) -> int:
+        ''' Retorna True se o objeto especificado for um nó e não uma folha.
+        '''
+        return self.component.IsFolder(node_key)
+
+    def IsFolderExpandable(self, node_key: str) -> int:
+        ''' Retorna True se a pasta pertencente ao nó especificado pode ser expandida.
+        '''
+        return self.component.IsFolderExpandable(node_key)
+
+    def IsFolderExpanded(self, node_key: str) -> int:
+        ''' Retorna True se a pasta pertencente ao nó especificado estiver expandida.
+        '''
+        return self.component.IsFolderExpanded(node_key)
+
+    def ItemContextMenu(self, node_key: str, item_name: str) -> None:
+        ''' Solicita um menu de contexto para um item.
+        '''
+        self.component.ItemContextMenu(node_key, item_name)
+
+    def NodeContextMenu(self, node_key: str) -> None:
+        ''' Solicita um menu de contexto para um nó.
+        '''
+        self.component.NodeContextMenu(node_key)
+
+    def PressButton(self, node_key: str, item_name: str) -> None:
+        ''' Emula o pressionamento de um botão.
+        '''
+        self.component.PressButton(node_key, item_name)
+
+    def PressHeader(self, header_name: str) -> None:
+        ''' Emula o clique em um cabeçalho.
+        '''
+        self.component.PressHeader(header_name)
+
+    def PressKey(self, key: str) -> None:
+        ''' Emula o pressionamento de uma tecla.
+        '''
+        self.component.PressKey(key)
+
+    def SelectColumn(self, column_name: str) -> None:
+        ''' Adiciona uma coluna à seleção de colunas, removendo a seleção de nós ou itens.
+        '''
+        self.component.SelectColumn(column_name)
+
+    def SelectedItemColumn(self) -> str:
+        ''' Retorna o nome da coluna do item selecionado.
+        '''
+        return self.component.SelectedItemColumn()
+
+    def SelectedItemNode(self) -> str:
+        ''' Retorna a chave do nó do item selecionado.
+        '''
+        return self.component.SelectedItemNode()
+
+    def SelectItem(self, node_key: str, item_name: str) -> None:
+        ''' Emula a seleção de um item, removendo todas as outras seleções.
+        '''
+        self.component.SelectItem(node_key, item_name)
+
+    def SelectNode(self, node_key: str) -> None:
+        ''' Adiciona o nó com a chave especificada à seleção de nós.
+        '''
+        self.component.SelectNode(node_key)
+
+    def SetCheckBoxState(self, node_key: str, item_name: str, state: int) -> None:
+        ''' Marca ou desmarca a caixa de seleção na célula especificada do controle de árvore.
+        Se o parâmetro "state" for igual a 0, a caixa de seleção é desmarcada; se o parâmetro for igual a 1, a caixa de seleção é marcada.
+        '''
+        self.component.SetCheckBoxState(node_key, item_name, state)
+
+    def SetColumnWidth(self, column_name: str, width: int) -> None:
+        ''' Define a largura de uma coluna em pixels.
+        '''
+        self.component.SetColumnWidth(column_name, width)
+
+    def UnselectAll(self) -> None:
+        ''' Remove todas as seleções.
+        '''
+        self.component.UnselectAll()
+
+    def UnselectColumn(self, column_name: str) -> None:
+        ''' Remove uma coluna da seleção de colunas.
+        '''
+        self.component.UnselectColumn(column_name)
+
+    def UnselectNode(self, node_key: str) -> None:
+        ''' Remove o nó com a chave especificada da seleção de nós.
+        '''
+        self.component.UnselectNode(node_key)
+
+    def ColumnOrder(self, column_order: object = None) -> object:
+        ''' A propriedade ColumnOrder é usada para trabalhar com a sequência de colunas.
+            O nome de cada coluna na árvore de colunas deve ocorrer exatamente uma vez.
+        '''
+        if column_order is not None:
+            self.component.ColumnOrder = column_order
+        return self.component.ColumnOrder
+
+    def HierarchyHeaderWidth(self, width: int = None) -> int:
+        ''' A largura do Hierarchy Header em pixels.
+        '''
+        if width is not None:
+            self.component.HierarchyHeaderWidth = width
+        return self.component.HierarchyHeaderWidth
+
+    def SelectedNode(self, node_key: str = None) -> str:
+        ''' Esta propriedade representa a chave do nó atualmente selecionado.
+            A seleção de um nó remove outras seleções (ou seja, seleção de coluna e seleção de item).
+        '''
+        if node_key is not None:
+            self.component.SelectedNode = node_key
+        return self.component.SelectedNode
+
+    def TopNode(self, top_node_key: str = None) -> str:
+        ''' Esta propriedade influencia a rolagem vertical do controle de árvore.
+            TopNode contém a chave do nó que está localizado na borda superior do controle de árvore.
+            A definição de um nó x como nó superior só é possível se houver nós visíveis suficientes abaixo de x para preencher a área de exibição do controle de árvore.
+        '''
+        if top_node_key is not None:
+            self.component.TopNode = top_node_key
+        return self.component.TopNode
 
 class SapGuiChart(SapGuiShell):
     ''' O objeto GuiChart é de natureza muito técnica. Deve ser utilizado apenas para gravação e reprodução,
