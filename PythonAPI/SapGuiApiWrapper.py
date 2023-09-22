567682af8a8cee3d6c83df918fd1ab21fb07c305
Nathan Almeida <neitan96@outlook.com>
2023-09-22 00:15:09 -0300

    Feita a classe GuiPicture para Python

--- PythonAPI/SapGuiApiWrapper.py
+++ PythonAPI/SapGuiApiWrapper.py
@@ -71,6 +71,7 @@ OK:
 	- GuiNetChart
 	- GuiOfficeIntegration
 	- GuiOkCodeField
+	- GuiPicture
 
 * Enumerations
 	- GuiComponentType
@@ -87,7 +88,6 @@ OK:
 TODO Components:
 
 * Objects:
-	- GuiPicture
 	- GuiStage
 	- GuiVHViewSwitch
 	- 
@@ -2029,6 +2029,79 @@ class SapGuiShell(SapGuiVContainer):
         ''' Informações adicionais de tipo para identificar o controle representado pelo shell, por exemplo Picture, TextEdit, GridView…
         '''
         return self.component.SubType
+
+class SapGuiPicture(SapGuiShell):
+    ''' O controle de imagem exibe uma imagem em uma tela SAP GUI.
+    '''
+
+    def Click(self) -> None:
+        ''' Emula um clique único em uma imagem.
+        '''
+        self.component.Click()
+
+    def ClickControlArea(self, x: int, y: int) -> None:
+        ''' Emula um clique em uma posição específica. As coordenadas devem ser fornecidas em pixels em relação ao controle da imagem conforme exibido na tela.
+        '''
+        self.component.ClickControlArea(x, y)
+
+    def ClickPictureArea(self, x: int, y: int) -> None:
+        ''' Emula um clique em uma posição específica. As coordenadas devem ser fornecidas em pixels em relação ao arquivo de imagem original. Elas podem diferir das coordenadas em pixels da imagem exibida devido ao dimensionamento.
+        '''
+        self.component.ClickPictureArea(x, y)
+
+    def ContextMenu(self, x: int, y: int) -> None:
+        ''' Abre um menu de contexto na posição especificada. As coordenadas devem ser fornecidas em pixels em relação ao controle da imagem conforme exibido na tela.
+        '''
+        self.component.ContextMenu(x, y)
+
+    def DoubleClick(self) -> None:
+        ''' Emula um clique duplo em uma imagem.
+        '''
+        self.component.DoubleClick()
+
+    def DoubleClickControlArea(self, x: int, y: int) -> None:
+        ''' Emula um clique duplo em uma posição específica. As coordenadas devem ser fornecidas em pixels
+        em relação ao controle da imagem conforme exibido na tela.
+        '''
+        self.component.DoubleClickControlArea(x, y)
+
+    def DoubleClickPictureArea(self, x: int, y: int) -> None:
+        ''' Emula um clique duplo em uma posição específica. As coordenadas devem ser fornecidas em pixels em
+        relação ao arquivo de imagem original. Elas podem diferir das coordenadas em pixels da imagem exibida devido ao dimensionamento.
+        '''
+        self.component.DoubleClickPictureArea(x, y)
+
+    def AltText(self) -> str:
+        ''' Esta propriedade contém o texto alternativo que pode ser atribuído a uma imagem (por exemplo, usado
+        para pessoas com deficiência visual quando um leitor de tela é usado) (somente leitura).
+        '''
+        return self.component.AltText
+
+    def DisplayMode(self) -> str:
+        ''' Modo de exibição da imagem (somente leitura).
+        Valores possíveis desta propriedade são:
+        - "Normal": A imagem é mostrada em seu tamanho original. Se o tamanho da imagem for maior do que o tamanho do controle,
+        o controle fornece barras de rolagem. Se o tamanho da imagem for menor do que o tamanho do controle, a imagem é mostrada
+        no canto superior esquerdo do controle.
+        - "Stretch": A imagem é redimensionada de forma que sempre ocupe a área completa do controle.
+        - "Fit": A imagem é redimensionada de forma que ela se ajusta à área de controle sem a necessidade de mostrar barras de rolagem.
+        Ao contrário de "Stretch", o modo "Fit" preserva a proporção de largura e altura da imagem.
+        - "NormalCenter": Semelhante a "Normal", exceto que a imagem não é mostrada no canto superior esquerdo, mas no centro do controle.
+        - "FitCenter": Semelhante a "Fit", exceto que a imagem não é mostrada no canto superior esquerdo, mas no centro do controle.
+        '''
+        return self.component.DisplayMode
+
+    def Icon(self) -> str:
+        ''' Retorna o código de ícone SAPGUI (por exemplo, "@01@") do ícone exibido. Se nenhum ícone for exibido, a propriedade contém
+        uma string vazia (somente leitura).
+        '''
+        return self.component.Icon
+
+    def Url(self) -> str:
+        ''' Retorna a URL da imagem exibida. Se um ícone estiver sendo exibido (consulte a propriedade "icon"), a propriedade contém uma
+        string vazia. Dependendo da aplicação que utiliza o controle, a URL pode conter partes temporárias da URL (por exemplo, UUIDs) (somente leitura).
+        '''
+        return self.component.Url
 
 class SapGuiOfficeIntegration(SapGuiShell):
     # TODO Criar descrição
